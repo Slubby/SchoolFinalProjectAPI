4d59caa0dec571922137d43440c3f34991276cdb
Pascal Huberts <pmhuberts@gmail.com>
2021-01-11 23:50:54 +0100

    policies

---
+++ app/Policies/Company/SupervisorPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Supervisor;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class SupervisorPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param User $user
+     * @param Supervisor $supervisor
+     * @return mixed
+     */
+    public function update(User $user, Supervisor $supervisor): bool
+    {
+        return $user->profile->id === $supervisor->company_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param User $user
+     * @param Supervisor $supervisor
+     * @return mixed
+     */
+    public function delete(User $user, Supervisor $supervisor): bool
+    {
+        return $user->profile->id === $supervisor->company_id;
+    }
+}
---
+++ app/Policies/Company/VacancyPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Vacancy;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VacancyPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param User $user
+     * @param Vacancy $vacancy
+     * @return mixed
+     */
+    public function update(User $user, Vacancy $vacancy): bool
+    {
+        return $user->profile->id === $vacancy->company_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param User $user
+     * @param Vacancy $vacancy
+     * @return mixed
+     */
+    public function delete(User $user, Vacancy $vacancy): bool
+    {
+        return $user->profile->id === $vacancy->company_id;
+    }
+}
